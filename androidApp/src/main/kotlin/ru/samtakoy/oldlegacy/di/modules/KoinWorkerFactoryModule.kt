39db1f510257aada67f175ea9eef3f74ad760208
Galimov Ruslan <samtakoy.first@gmail.com>
2025-12-08 14:54:44 +0300

    удалил legacy из индекса git

--- androidApp/src/main/kotlin/ru/samtakoy/oldlegacy/di/modules/KoinWorkerFactoryModule.kt
+++
@@ -1,9 +0,0 @@
-package ru.samtakoy.oldlegacy.di.modules
-
-import org.koin.androidx.workmanager.factory.KoinWorkerFactory
-import org.koin.dsl.module
-
-// TODO не используется?
-fun workerFactoryModule() = module {
-    single { KoinWorkerFactory() }
-}
